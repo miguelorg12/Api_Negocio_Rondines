d7c6fcfc8956489d8dfb6d5ea394a4dc8d893043
Fernando <fgolmos10@gmail.com>
2025-07-29 19:18:53 -0600

    ✨feat: update database host configuration to use environment variables for improved flexibility

--- src/configs/data-source.ts
+++ src/configs/data-source.ts
@@ -21,7 +21,7 @@ import { Code } from "@entities/code.entity";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
